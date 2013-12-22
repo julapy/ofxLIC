09c2ff3fb80a98167644e9d92facf39f07332ace
lukasz karluk <julapy@googlemail.com>
2013-12-22 14:45:44 +1100

    fixed formatting.

--- src/ofxStreamline.cpp
+++ src/ofxStreamline.cpp
@@ -8,352 +8,331 @@
 
 #include "ofxStreamline.h"
 
-ofxStreamline :: ofxStreamline ()
-{
-    setVectorData( NULL, 0, 0 );
-    setPosition( 0, 0 );
-    setStep( 1.0 );
+ofxStreamline::ofxStreamline() {
+    setVectorData(NULL, 0, 0);
+    setPosition(0, 0);
+    setStep(1.0);
     
-    stepNum     = 10;
-    pulsePhase  = 0;
-    pulseSpeed  = 0.3;
-    colorPhase  = 0;
-    colorSpeed  = 0.01;
-    dotSize     = 5;
-    stripWidth  = 3;
+    stepNum = 10;
+    pulsePhase = 0;
+    pulseSpeed = 0.3;
+    colorPhase = 0;
+    colorSpeed = 0.01;
+    dotSize = 5;
+    stripWidth = 3;
 }
 
-ofxStreamline :: ~ofxStreamline ()
-{
+ofxStreamline::~ofxStreamline() {
     //
 }
 
 //////////////////////////////////////////////////
 //  SETUP.
 //////////////////////////////////////////////////
-
-void ofxStreamline :: setup ()
-{
+void ofxStreamline::setup() {
     //
 }
 
-void ofxStreamline :: setVectorData ( ofPoint* vecField, int vecFieldW, int vecFieldH )
-{
-    this->vecField  = vecField;
-    this->width     = vecFieldW;
-    this->height    = vecFieldH;
+void ofxStreamline::setVectorData(ofVec2f * vecField, int vecFieldW, int vecFieldH) {
+    this->vecField = vecField;
+    this->width = vecFieldW;
+    this->height = vecFieldH;
 }
 
-void ofxStreamline :: setPosition ( float x, float y )
-{
+void ofxStreamline::setPosition(float x, float y) {
     pos.x = x;
     pos.y = y;
 }
 
-void ofxStreamline :: setStep ( float step )
-{
-    this->step  = step;
-    dotSize     = step * 0.5;
+void ofxStreamline::setStep(float step) {
+    this->step = step;
+    dotSize = step * 0.5;
 }
-
 
 //////////////////////////////////////////////////
 //  UPDATE.
 //////////////////////////////////////////////////
 
-void ofxStreamline :: update ()
-{
-    ofxVec2f v;
+void ofxStreamline::update() {
+    ofVec2f v;
     int s = stepNum;
     int t = s * 2 + 1;
     
     points.clear();
-    points.assign( t, ofPoint() );
+    points.assign(t, ofVec2f());
 
-    points[ s ].x = pos.x;          // center point.
-    points[ s ].y = pos.y;
+    points[s].x = pos.x;          // center point.
+    points[s].y = pos.y;
     
     v = pos;
-    for( int i=0; i<s; i++ )        // move backwards.
-    {
-        RK( v, -step );
+    for(int i=0; i<s; i++) {        // move backwards.
+        RK(v, -step);
         
         int j = s - 1 - i;
-        points[ j ].x = v.x;
-        points[ j ].y = v.y;
+        points[j].x = v.x;
+        points[j].y = v.y;
     }
     
     v = pos;
-    for( int i=0; i<s; i++ )       // move forwards.
-    {
-        RK( v, step );
+    for(int i=0; i<s; i++) {      // move forwards.
+        RK(v, step);
         
         int j = s + 1 + i;
-        points[ j ].x = v.x;
-        points[ j ].y = v.y;
+        points[j].x = v.x;
+        points[j].y = v.y;
     }
 }
 
-void ofxStreamline :: RK ( ofxVec2f &p, double h )
-{
-    ofxVec2f v;
-    ofxVec2f k1, k2, k3, k4;
+void ofxStreamline::RK(ofVec2f & p, double h) {
+    ofVec2f v;
+    ofVec2f k1, k2, k3, k4;
     
-    v = getVector( p );
+    v = getVector(p);
     v.normalize();
     
     k1 = v * h;
-    v = getVector( p + k1 * 0.5 );
+    v = getVector(p + k1 * 0.5);
     v.normalize();
     
     k2 = v * h;
-    v = getVector( p + k2 * 0.5 );
+    v = getVector(p + k2 * 0.5);
     v.normalize();
     
     k3 = v * h;
-    v = getVector( p + k3 );
+    v = getVector(p + k3);
     v.normalize();
     
     k4 = v * h;
     p += k1/6 + k2/3 + k3/3 + k4/6;
 }
 
-ofxVec2f ofxStreamline :: getVector( const ofPoint &p )
-{
-    ofxVec2f v;
+ofVec2f ofxStreamline::getVector(const ofVec2f & p) {
+    ofVec2f v;
     
     int x, y;
-    x = ( (int)p.x + width  ) % width;
-    y = ( (int)p.y + height ) % height;
+    x = ((int)p.x + width) % width;
+    y = ((int)p.y + height) % height;
     
-    if( x < 0 || x >= width && y < 0 && y >= height )
+    if((x < 0)      ||
+       (x >= width) ||
+       (y < 0)      ||
+       (y >= height)) {
         return v;
+    }
     
     int i = y * width + x;
         
-    return ofxVec2f( vecField[ i ].x, vecField[ i ].y );
+    return ofVec2f(vecField[i].x, vecField[i].y);
 }
 
 //////////////////////////////////////////////////
 //  DRAW.
 //////////////////////////////////////////////////
 
-void ofxStreamline :: draw ()
-{
-//    drawLines( true );
-    drawStrip( true );
-//    drawDots();
-}
-
-void ofxStreamline :: drawLines ( bool bSmooth )
-{
-    if( bSmooth )
-    {
-        glHint( GL_LINE_SMOOTH_HINT, GL_NICEST );
-        glEnable( GL_LINE_SMOOTH );
+void ofxStreamline::drawLines(bool bSmooth) {
+    if(bSmooth) {
+        glHint(GL_LINE_SMOOTH_HINT, GL_NICEST);
+        glEnable(GL_LINE_SMOOTH);
     }
     
     int t = points.size();
     
-    float* vert = new float[ t * 3 ];
-    float* colr = new float[ t * 4 ];
+    float* vert = new float[t * 3];
+    float* colr = new float[t * 4];
     
-    for( int i=0; i<t; i++ )
-    {
-        vert[ i * 3 + 0 ] = points[ i ].x;
-        vert[ i * 3 + 1 ] = points[ i ].y;
-        vert[ i * 3 + 2 ] = 0;
+    for(int i=0; i<t; i++) {
+        vert[i * 3 + 0] = points[i].x;
+        vert[i * 3 + 1] = points[i].y;
+        vert[i * 3 + 2] = 0;
         
-        colr[ i * 4 + 0 ] = 0;
-        colr[ i * 4 + 1 ] = 0;
-        colr[ i * 4 + 2 ] = 0;
-        colr[ i * 4 + 3 ] = 255;
+        colr[i * 4 + 0] = 0;
+        colr[i * 4 + 1] = 0;
+        colr[i * 4 + 2] = 0;
+        colr[i * 4 + 3] = 255;
     }
     
-    glEnableClientState( GL_COLOR_ARRAY );
-    glColorPointer( 4, GL_FLOAT, 0, colr );
+    glEnableClientState(GL_COLOR_ARRAY);
+    glColorPointer(4, GL_FLOAT, 0, colr);
     
-    glEnableClientState( GL_VERTEX_ARRAY );		
-    glVertexPointer( 3, GL_FLOAT, 0, vert );
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glVertexPointer(3, GL_FLOAT, 0, vert);
     
-    glDrawArrays( GL_LINE_STRIP, 0, t );
+    glDrawArrays(GL_LINE_STRIP, 0, t);
     
-    glDisableClientState( GL_COLOR_ARRAY );
-    glDisableClientState( GL_VERTEX_ARRAY );
+    glDisableClientState(GL_COLOR_ARRAY);
+    glDisableClientState(GL_VERTEX_ARRAY);
     
     delete[] vert;
     delete[] colr;
 }
 
-void ofxStreamline :: drawStrip ( bool bSmooth )
-{
-    if( bSmooth )
-    {
-        glHint( GL_LINE_SMOOTH_HINT, GL_NICEST );
-        glEnable( GL_LINE_SMOOTH );
+void ofxStreamline::drawStrip(bool bSmooth) {
+    if(bSmooth) {
+        glHint(GL_LINE_SMOOTH_HINT, GL_NICEST);
+        glEnable(GL_LINE_SMOOTH);
     }
     
     int t = points.size();
     
-    float* vert = new float[ t * 3 * 2 ];
-    float* colr = new float[ t * 4 * 2 ];
+    float* vert = new float[t * 3 * 2];
+    float* colr = new float[t * 4 * 2];
 
     float* vert_line = NULL;
     float* colr_line = NULL;
     
-    if( bSmooth )
-    {
-        vert_line = new float[ t * 3 * 2 ];
-        colr_line = new float[ t * 4 * 2 ];
+    if(bSmooth) {
+        vert_line = new float[t * 3 * 2];
+        colr_line = new float[t * 4 * 2];
     }
     
     int j, k;
     
-    for( int i=0; i<t; i++ )
-    {
+    for(int i=0; i<t; i++) {
         float s;
         
         //--- envelope.
         
         float e;
-        e = sin( ( i / (float)( t - 1 ) ) * PI );
+        e = sin((i/(float)(t - 1)) * PI);
         
         //--- pulse phase.
         
         float p;
-        p = sin( ( ofGetFrameNum() + i ) * pulseSpeed + pulsePhase );
+        p = sin((ofGetFrameNum() + i) * pulseSpeed + pulsePhase);
         p += 1;
         p *= 0.5;
 
         s = 0.8;      // trucate.
         p *= s;
-        p += ( 1 - s ) * 0.5;
+        p += (1 - s) * 0.5;
         
         //--- color phase.
         
         float c;
-        c = sin( ( ofGetFrameNum() - i ) * colorSpeed + colorPhase );
+        c = sin((ofGetFrameNum() - i) * colorSpeed + colorPhase);
         c += 1;
         c *= 0.5;
         
         s = 0.6;      // trucate.
         c *= s;
-        c += ( 1 - s ) * 0.5;
+        c += (1 - s) * 0.5;
         
          //--- points.
         
-        j = MAX( i - 1, 0 );
+        j = MAX(i - 1, 0);
         
-        ofxVec2f p1 = ofxVec2f( points[ i ] );
-        ofxVec2f p2 = ofxVec2f( points[ j ] );
+        ofVec2f p1 = ofVec2f(points[i]);
+        ofVec2f p2 = ofVec2f(points[j]);
         
-        ofxVec2f v = p1 - p2;
+        ofVec2f v = p1 - p2;
         v.perpendicular();
         v.normalize();
         v *= stripWidth * e * p;
         
-        vert[ i * 3 * 2 + 0 ] = points[ i ].x + v.x;
-        vert[ i * 3 * 2 + 1 ] = points[ i ].y + v.y;
-        vert[ i * 3 * 2 + 2 ] = 0;
-        vert[ i * 3 * 2 + 3 ] = points[ i ].x - v.x;
-        vert[ i * 3 * 2 + 4 ] = points[ i ].y - v.y;
-        vert[ i * 3 * 2 + 5 ] = 0;
+        vert[i * 3 * 2 + 0] = points[i].x + v.x;
+        vert[i * 3 * 2 + 1] = points[i].y + v.y;
+        vert[i * 3 * 2 + 2] = 0;
+        vert[i * 3 * 2 + 3] = points[i].x - v.x;
+        vert[i * 3 * 2 + 4] = points[i].y - v.y;
+        vert[i * 3 * 2 + 5] = 0;
         
-        colr[ i * 4 * 2 + 0 ] = c;
-        colr[ i * 4 * 2 + 1 ] = c;
-        colr[ i * 4 * 2 + 2 ] = c;
-        colr[ i * 4 * 2 + 3 ] = 1.0;
-        colr[ i * 4 * 2 + 4 ] = c;
-        colr[ i * 4 * 2 + 5 ] = c;
-        colr[ i * 4 * 2 + 6 ] = c;  
-        colr[ i * 4 * 2 + 7 ] = 1.0;
+        colr[i * 4 * 2 + 0] = c;
+        colr[i * 4 * 2 + 1] = c;
+        colr[i * 4 * 2 + 2] = c;
+        colr[i * 4 * 2 + 3] = 1.0;
+        colr[i * 4 * 2 + 4] = c;
+        colr[i * 4 * 2 + 5] = c;
+        colr[i * 4 * 2 + 6] = c;
+        colr[i * 4 * 2 + 7] = 1.0;
         
-        if( !bSmooth )
+        if(bSmooth == false) {
             continue;
+        }
         
         j = i * 3;
         k = t * 3 * 2 - 3 - j;
         
-        vert_line[ j + 0 ] = points[ i ].x + v.x;
-        vert_line[ j + 1 ] = points[ i ].y + v.y;
-        vert_line[ j + 2 ] = 0;
-        vert_line[ k + 0 ] = points[ i ].x - v.x;
-        vert_line[ k + 1 ] = points[ i ].y - v.y;
-        vert_line[ k + 2 ] = 0;
+        vert_line[j + 0] = points[i].x + v.x;
+        vert_line[j + 1] = points[i].y + v.y;
+        vert_line[j + 2] = 0;
+        vert_line[k + 0] = points[i].x - v.x;
+        vert_line[k + 1] = points[i].y - v.y;
+        vert_line[k + 2] = 0;
 
         j = i * 4;
         k = t * 4 * 2 - 4 - j;
         
-        colr_line[ j + 0 ] = c;
-        colr_line[ j + 1 ] = c;
-        colr_line[ j + 2 ] = c;
-        colr_line[ j + 3 ] = 1.0;
-        colr_line[ k + 0 ] = c;
-        colr_line[ k + 1 ] = c;
-        colr_line[ k + 2 ] = c;  
-        colr_line[ k + 3 ] = 1.0;
+        colr_line[j + 0] = c;
+        colr_line[j + 1] = c;
+        colr_line[j + 2] = c;
+        colr_line[j + 3] = 1.0;
+        colr_line[k + 0] = c;
+        colr_line[k + 1] = c;
+        colr_line[k + 2] = c;
+        colr_line[k + 3] = 1.0;
     }
     
     //---
     
-	glEnableClientState( GL_COLOR_ARRAY );
-	glColorPointer( 4, GL_FLOAT, 0, colr );
+	glEnableClientState(GL_COLOR_ARRAY);
+	glColorPointer(4, GL_FLOAT, 0, colr);
 	
-	glEnableClientState( GL_VERTEX_ARRAY );	
-	glVertexPointer( 3, GL_FLOAT, 0, vert );
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glVertexPointer(3, GL_FLOAT, 0, vert);
 	
-	glDrawArrays( GL_QUAD_STRIP, 0, t * 2 );
+	glDrawArrays(GL_QUAD_STRIP, 0, t * 2);
 	
-	glDisableClientState( GL_COLOR_ARRAY );
-	glDisableClientState( GL_VERTEX_ARRAY );
+	glDisableClientState(GL_COLOR_ARRAY);
+	glDisableClientState(GL_VERTEX_ARRAY);
 
     delete[] vert;
     delete[] colr;
     
     //---
     
-    if( !bSmooth )
+    if(bSmooth == false) {
         return;
+    }
     
-	glEnableClientState( GL_COLOR_ARRAY );
-	glColorPointer( 4, GL_FLOAT, 0, colr_line );
+	glEnableClientState(GL_COLOR_ARRAY);
+	glColorPointer(4, GL_FLOAT, 0, colr_line);
 	
-	glEnableClientState( GL_VERTEX_ARRAY );	
-	glVertexPointer( 3, GL_FLOAT, 0, vert_line );
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glVertexPointer(3, GL_FLOAT, 0, vert_line);
 	
-	glDrawArrays( GL_LINE_STRIP, 0, t * 2 );
+	glDrawArrays(GL_LINE_STRIP, 0, t * 2);
 	
-	glDisableClientState( GL_COLOR_ARRAY );
-	glDisableClientState( GL_VERTEX_ARRAY );
+	glDisableClientState(GL_COLOR_ARRAY);
+	glDisableClientState(GL_VERTEX_ARRAY);
 
     delete[] vert_line;
     delete[] colr_line;
 }
 
-void ofxStreamline :: drawDots ()
-{
+void ofxStreamline::drawDots() {
     float s = step * 0.5;
     
     int t = points.size();
-    for( int i=0; i<t; i++ )
-    {
+    for(int i=0; i<t; i++) {
         float p;
-        p = sin( ( ofGetFrameNum() + i ) * pulseSpeed + pulsePhase );
+        p = sin((ofGetFrameNum() + i) * pulseSpeed + pulsePhase);
         p += 1;
         p *= 0.5;
-        p = ofClamp( p, 0.2, 1.0 );
+        p = ofClamp(p, 0.2, 1.0);
         
         float c;
-        c = sin( ( ofGetFrameNum() - i ) * colorSpeed + colorPhase );
+        c = sin((ofGetFrameNum() - i) * colorSpeed + colorPhase);
         c += 1;
         c *= 0.5;
 
-        ofSetColor( 255 * c, 255 * c, 255 * c, 255 );
+        ofSetColor(255 * c, 255);
         
         ofFill();
-        ofCircle( points[ i ].x, points[ i ].y, s * p );
+        ofCircle(points[i].x, points[i].y, s * p);
         
         ofNoFill();
-        ofCircle( points[ i ].x, points[ i ].y, s * p );
+        ofCircle(points[i].x, points[i].y, s * p);
     }
+    
+    ofSetColor(255);
 }
--- src/ofxStreamline.h
+++ src/ofxStreamline.h
@@ -1,53 +1,45 @@
 //
 //  ofxStreamline.h
-//  opencvExample
-//
 //  Created by lukasz karluk on 22/03/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
 
 #pragma once
 
 #include "ofMain.h"
-#include "ofxVec2f.h"
 
-class ofxStreamline : public ofBaseApp
-{
+class ofxStreamline : public ofBaseApp {
   
 public:
     
-     ofxStreamline();
+    ofxStreamline();
     ~ofxStreamline();
     
-    void        setup   ();
-    void        update  ();
-    void        draw    ();
+    void setup();
+    void update();
+    void drawLines(bool bSmooth = false);
+    void drawStrip(bool bSmooth = false);
+    void drawDots();
     
-    void        drawLines       ( bool bSmooth = false );
-    void        drawStrip       ( bool bSmooth = false );
-    void        drawDots        ();
+    void setVectorData(ofVec2f * vecField, int vecFieldW, int vecFieldH);
+    void setPosition(float x, float y);
+    void setStep(float step);
     
-    void        setVectorData   ( ofPoint* vecField, int vecFieldW, int vecFieldH );
-    void        setPosition     ( float x, float y );
-    void        setStep         ( float step );
+    void RK(ofVec2f & p, double h);
+    ofVec2f getVector(const ofVec2f & p);
     
-    void        RK              ( ofxVec2f &p, double h );
-    ofxVec2f    getVector       ( const ofPoint &p );
+    ofVec2f * vecField;
+    int width;
+    int height;
     
-    ofPoint*        vecField;
-    int             width;
-    int             height;
+    ofVec2f pos;
+    vector<ofVec2f> points;
+    float step;
+    int stepNum;
     
-    ofxVec2f        pos;
-    vector<ofPoint> points;
-    float           step;
-    int             stepNum;
-    
-    float           dotSize;
-    float           stripWidth;
-    float           pulsePhase;
-    float           pulseSpeed;
-    float           colorPhase;
-    float           colorSpeed;
-    
+    float dotSize;
+    float stripWidth;
+    float pulsePhase;
+    float pulseSpeed;
+    float colorPhase;
+    float colorSpeed;
 };
